4949907255e9a8d8bb5304decca7b16d36df153c
Tatyana <tatyana_strelkova@i.ua>
2022-05-17 17:52:28 +0300

    fixed homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,67 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+/* 
+5. Замена значения атрибута src элемента <img> в модальном окне 
+перед открытием. Используй готовую разметку модального окна 
+с изображением из примеров библиотеки basicLightbox. */
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const containerWithGalleryItems = document.querySelector('.gallery');
+const markUp = createGalleryMarkUp(galleryItems);
+
+containerWithGalleryItems.insertAdjacentHTML('afterbegin', markUp)
+containerWithGalleryItems.addEventListener('click', onGalleryItemsClick)
+
+function createGalleryMarkUp(galleryItems) {
+    return galleryItems.
+        map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+        })
+        .join('');
+}
+ 
+function onGalleryItemsClick(e) {
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+
+    /*     e.target.src = e.target.dataset.source; */
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`)
+    instance.show()
+   
+}
+
+window.addEventListener('keydown', closeModalPressEsc)
+
+/* window.addEventListener('keydown', e => { 
+    if (e.key === "Escape") { 
+        instance.close();
+    }
+}) */
+
+function closeModalPressEsc(e) {
+  if (e.key === "Escape") {
+    instance.close()
+  }
+}
+
+
+ 
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+/* Сделай такую же галерею как в первом задании, но используя библиотеку
+SimpleLightbox, которая возьмет на себя обработку кликов по изображениям,
+открытие и закрытие модального окна, а также пролистывание изображений 
+при помощи клавиатуры. 
+Посмотри демо видео работы галереи с подключенной библиотекой. 
 
-console.log(galleryItems);
+2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. 
+3. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и 
+simple-lightbox.min.css.
+4. Инициализация библиотеки после того как элементы галереи созданы и 
+добавлены в div.gallery. Для этого ознакомься с документацией SimpleLightbox - 
+в первую очередь секции «Usage» и «Markup».
+5. Посмотри в документации секцию «Options» и добавь отображение подписей 
+к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 
+250 миллисекунд после открытия изображения.*/
+
+import { galleryItems } from './gallery-items.js';
+
+const containerWithGalleryItems = document.querySelector('.gallery');
+const markUp = createGalleryMarkUp(galleryItems);
+
+containerWithGalleryItems.insertAdjacentHTML('afterbegin', markUp)
+containerWithGalleryItems.addEventListener('click', onGalleryItemsClick)
+
+function createGalleryMarkUp(galleryItems) {
+    return galleryItems.
+        map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+        })
+        .join('');
+}
+
+function onGalleryItemsClick(e) {
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {    });
+    
+}
+
