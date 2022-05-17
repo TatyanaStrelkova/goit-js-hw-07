33c03ca0ec4f28b6ecf0ab9d5db4cbd95e6d05ce
Tatyana <tatyana_strelkova@i.ua>
2022-05-17 22:10:37 +0300

    fixed js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -20,25 +20,28 @@ const containerWithGalleryItems = document.querySelector('.gallery');
 const markUp = createGalleryMarkUp(galleryItems);
 
 containerWithGalleryItems.insertAdjacentHTML('afterbegin', markUp)
-containerWithGalleryItems.addEventListener('click', onGalleryItemsClick)
+containerWithGalleryItems.addEventListener('click', () => {
+    const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, });
+})
 
 function createGalleryMarkUp(galleryItems) {
     return galleryItems.
         map(({ preview, original, description }) => {
             return `
             <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
 </a>
 `;
         })
         .join('');
 }
 
-function onGalleryItemsClick(e) {
+/* function onGalleryItemsClick(e) {
     if (e.target.nodeName !== "IMG") {
         return;
     }
-    const lightbox = new SimpleLightbox('.gallery a', {    });
+} */
+    /* const lightbox = new SimpleLightbox('.gallery a', {  captionDelay: 250,  }); */
     
-}
+
 
