07a44cd2e127ac51bbc36f354e79eb037539edde
Tatyana <tatyana_strelkova@i.ua>
2022-05-18 16:27:27 +0300

    add some features

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,13 @@ import { galleryItems } from './gallery-items.js';
 
 const containerWithGalleryItems = document.querySelector('.gallery');
 const markUp = createGalleryMarkUp(galleryItems);
+const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
 
-containerWithGalleryItems.insertAdjacentHTML('afterbegin', markUp)
-containerWithGalleryItems.addEventListener('click', onGalleryItemsClick)
+containerWithGalleryItems.insertAdjacentHTML('afterbegin', markUp);
+containerWithGalleryItems.addEventListener('click', onGalleryItemsClick);
+/* window.addEventListener('keydown', onEscKeyPress); */
 
 function createGalleryMarkUp(galleryItems) {
     return galleryItems.
@@ -24,27 +28,26 @@ function createGalleryMarkUp(galleryItems) {
         })
         .join('');
 }
- 
+
 function onGalleryItemsClick(e) {
     if (e.target.nodeName !== "IMG") {
         return;
     }
-
-    /*     e.target.src = e.target.dataset.source; */
-    const instance = basicLightbox.create(`
-    <img src="${e.target.dataset.source}" width="800" height="600">
-`)
-    instance.show()
-   
+  instance.show()   
+  
 }
 
-window.addEventListener('keydown', closeModalPressEsc)
-
-function closeModalPressEsc(e) {
-  if (e.key === "Escape") {
-    instance.close()
+/* function onEscKeyPress(event) {
+  if (event.key === "Escape") {
+    instance.close();
   }
-}
+}  */
+
+
+
+
+
+
 
 
  
